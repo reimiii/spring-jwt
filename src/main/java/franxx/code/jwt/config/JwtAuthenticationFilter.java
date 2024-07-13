377faca9f779813bec668ab395cca18f81404b5e
Hilmi AM <imiia75775@proton.me>
2024-07-13 08:38:59 +0700

    config jwt

---
+++ src/main/java/franxx/code/jwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package franxx.code.jwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain
+  ) throws ServletException, IOException {
+
+  }
+}
