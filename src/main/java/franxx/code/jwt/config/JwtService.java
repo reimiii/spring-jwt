9982160ff68ae73d87c52fc020b2c7ace6f610eb
Hilmi AM <imiia75775@proton.me>
2024-07-13 09:42:20 +0700

    generate some key

--- src/main/java/franxx/code/jwt/config/JwtService.java
+++ src/main/java/franxx/code/jwt/config/JwtService.java
@@ -1,10 +1,32 @@
 package franxx.code.jwt.config;
 
-import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.security.Key;
+
+@Service
 public class JwtService {
+
+  private static final String SECRET_KEY = "1F19BF74365EE672AB05AE3E85F5F46B";
+
   public String extractUsername(String token) {
     return null;
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 }
