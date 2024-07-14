3e64592da395e33eb39a7e3b2232e77cde8be7dc
Hilmi AM <imiia75775@proton.me>
2024-07-14 17:20:11 +0700

    create auth controller

---
+++ src/main/java/franxx/code/jwt/auth/AuthController.java
@@ -0,0 +1,21 @@
+package franxx.code.jwt.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthResponse> register(
+      @RequestBody RegisterRequest request
+  ) {
+    // meh
+  }
+}
