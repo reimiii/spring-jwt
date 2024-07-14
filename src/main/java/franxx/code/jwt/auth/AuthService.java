66c54cea0e073cb393dd774479eaef9fd60a340b
Hilmi AM <imiia75775@proton.me>
2024-07-14 17:50:00 +0700

    implement register

--- src/main/java/franxx/code/jwt/auth/AuthService.java
+++ src/main/java/franxx/code/jwt/auth/AuthService.java
@@ -1,16 +1,33 @@
 package franxx.code.jwt.auth;
 
+import franxx.code.jwt.config.JwtService;
+import franxx.code.jwt.user.Role;
+import franxx.code.jwt.user.User;
+import franxx.code.jwt.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-
+  private final UserRepository repository;
+  private final PasswordEncoder encoder;
+  private final JwtService jwtService;
 
   public AuthResponse register(RegisterRequest request) {
+    var user = User.builder()
+        .firstName(request.getFirstName())
+        .lastName(request.getLastName())
+        .email(request.getEmail())
+        .password(encoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
 
-    return null;
+    repository.save(user);
+    String token = jwtService.generateToken(user);
+
+    return AuthResponse.builder().token(token).build();
   }
 
   public AuthResponse authenticate(AuthRequest request) {
