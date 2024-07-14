c31a1d2165182a90c148e1419177e716d20f377e
Hilmi AM <imiia75775@proton.me>
2024-07-14 17:34:43 +0700

    create response request

---
+++ src/main/java/franxx/code/jwt/auth/AuthRequest.java
@@ -0,0 +1,15 @@
+package franxx.code.jwt.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+  private String email;
+  String password;
+}
